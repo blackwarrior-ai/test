a5321858474f147f4a46242c584be8ff58f97e22
User <user@example.com>
2026-04-08 19:54:47 +0500

    Removed the duplicate/wrong 2nd movable box section (MobileProductSlider) from mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,6 @@ export default function Home() {
           <HeroBanner />
           <MobileSmallCategoryCarousel />
           <MobileMarqueeBanner />
-          <MobileProductSlider />
           <MobileCategoryCarousel />
           <MobileBestSellers />
           <MobileFlashSale />
